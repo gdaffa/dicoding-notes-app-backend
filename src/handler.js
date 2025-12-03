5ab9559f3b1382cec41b66db8a6eca8968d860f8
Gibran <gibrandaffa92@gmail.com>
2025-12-03 17:31:32 +0700

    feat(handler): Http request handler

--- src/handler.js
+++ src/handler.js
@@ -1,12 +1,105 @@
+import Hapi from "@hapi/hapi";
+import { randstr } from './utils.js';
+
+/**
+ * @type {Map<string, { title: string; tags: string[]; body: string; createdAt: string; updatedAt: string; }>}
+ */
 let notes = new Map();
 
-function httpGetNote() {}
+function getNotesAsObject() {
+   return [...notes].map(([ id, note ]) => ({ id, ...note }));
+}
 
-function httpAddNote() {}
+/**
+ * @param {Hapi.ResponseToolkit} h
+ * @param {string} status
+ * @param {number} code
+ * @param {message} message
+ * @param {object} options
+ */
+function sendResponse(h, status, code, message, options) {
+   let res = { status, code, message, ...options };
+   h.response().code(code);
+   return res;
+}
 
-function httpChangeNote() {}
+/**
+ * @type {Hapi.Lifecycle.Method}
+ */
+function httpGetNote(req, h) {
+   let { id } = req.params;
+   let data;
 
-function httpDeleteNote() {}
+   if (id == undefined) {
+      data = { notes: getNotesAsObject()};
+   }
+   else {
+      if (!notes.has(id)) {
+         let message = `Catatan dengan id ${id} tidak ditemukan.`
+         return sendResponse(h, 'success', 400, message);
+      }
+      data = { note: { id, ...notes.get(id) } };
+   }
+
+   data = { data };
+   return sendResponse(h, 'success', 200, 'Catatan berhasil diambil.', data);
+};
+
+/**
+ * @type {Hapi.Lifecycle.Method}
+ */
+function httpAddNote(req, h) {
+   let { title, tags, body } = req.payload;
+   let currentDate           = (new Date()).toISOString();
+
+   let id;
+   do {
+      id = randstr(16);
+   } while(!notes.has(id));
+
+   let newNote = {
+      title, tags, body,
+      createdAt : currentDate,
+      updatedAt : currentDate
+   }
+   notes.set(id, newNote);
+
+   let data = { data: { noteId: id } };
+   return sendResponse(h, 'created', 201, 'Catatan berhasil dibuat.', data);
+};
+
+/**
+ * @type {Hapi.Lifecycle.Method}
+ */
+function httpChangeNote(req, h) {
+   let { id } = req.params;
+   let { title, tags, body } = req.payload;
+
+   if (!notes.has(id)) {
+      let message = `Catatan dengan id ${id} tidak ditemukan.`
+      return sendResponse(h, 'success', 400, message);
+   }
+
+   let note    = notes.get(id);
+   let newNote = {
+      title, tags, body,
+      updatedAt : (new Date()).toISOString()
+   }
+   Object.assign(note, newNote);
+
+   return sendResponse(h, 'success', 200, 'Catatan berhasil diubah.');
+};
+
+/**
+ * @type {Hapi.Lifecycle.Method}
+ */
+function httpDeleteNote(req, h) {
+   let { id } = req.params;
+   if (notes.has(id)) {
+      notes.delete(id);
+   }
+   return sendResponse(h, 'success', 200, 'Catatan berhasil dihapus.');
+};
 
 export {
    notes,
